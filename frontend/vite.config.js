2c5d3a1fc27d520d7a64828ae56d7bc97452a568
SIMRAN <118015518+deepsimrantechie@users.noreply.github.com>
2025-01-09 17:57:48 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5174 },
+
+  // Optional: Custom server configuration
+  server: {
+    port: 5174, // Custom port for your development server
+  },
 });
